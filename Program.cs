23f216839789b15c337620d3845de14c7ba930f7
akos500 <akos500@yandex.ru>
2022-09-19 22:31:44 +0300

    Числа четные от 1 до N

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, принимающая на вход число N и на выходе показывает
+// все четные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+   
+if (result == 1)
+
+{
+    result += 1;
+}
+
+while (result <= num)
+
+{
+    Console.WriteLine(result);
+    result += 2;
+}
